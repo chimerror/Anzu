3610db2d870c783c46bae3b6eefba19ffb07ed08
foobardog <foobardog@hotmail.com>
2015-03-08 19:23:12 -0700

    First implementation of Honcho Notification Tray Program.

---
+++ Anzu/Honcho/HonchoApplicationContext.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Drawing;
+using System.Linq;
+using System.Reflection;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Honcho
+{
+
+    internal class HonchoApplicationContext : ApplicationContext
+    {
+        private readonly IContainer _components;
+        private readonly NotifyIcon _notifyIcon;
+
+        public HonchoApplicationContext()
+        {
+            _components = new Container();
+            _notifyIcon = new NotifyIcon(_components)
+            {
+                ContextMenuStrip = new ContextMenuStrip(),
+                Icon = new Icon("coloredcaptalistpicture.ico"),
+                Text = "Anzu Honcho",
+                Visible = true
+            };
+
+            _notifyIcon.MouseUp += NotifyIcon_MouseUp;
+            _notifyIcon.ContextMenuStrip.Opening += ContextMenuStrip_Opening;
+        }
+
+        private void NotifyIcon_MouseUp(object sender, MouseEventArgs e)
+        {
+            // HACK: This will make the menu display on a left click as well as a right
+            // click. Using reflection is grody, but it seems to be the easiest way.
+            if (e.Button == MouseButtons.Left)
+            {
+                var showContextMenuMethodInfo = typeof(NotifyIcon).GetMethod("ShowContextMenu", BindingFlags.Instance | BindingFlags.NonPublic);
+                showContextMenuMethodInfo.Invoke(_notifyIcon, null);
+            }
+        }
+
+        private void ContextMenuStrip_Opening(object sender, CancelEventArgs e)
+        {
+            e.Cancel = false;
+
+            var exitMenuItem = new ToolStripMenuItem("&Exit", null, Exit_OnClicked);
+            _notifyIcon.ContextMenuStrip.Items.Add(exitMenuItem);
+        }
+
+        private void Exit_OnClicked(object sender, EventArgs e)
+        {
+            ExitThread();
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            base.Dispose(disposing);
+            if (disposing && _components != null)
+            {
+                _components.Dispose();
+            }
+        }
+
+        protected override void ExitThreadCore()
+        {
+            _notifyIcon.Visible = false;
+            base.ExitThreadCore();
+        }
+    }
+}
