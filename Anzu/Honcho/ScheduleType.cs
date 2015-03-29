7b7addce86bb24caa6e2c7771ac2a1acaa367837
foobardog <foobardog@hotmail.com>
2015-03-28 21:23:50 -0700

    Actually adding ScheduleType. Don't know how git missed this.

---
+++ Anzu/Honcho/ScheduleType.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Honcho
+{
+    internal enum ScheduleType
+    {
+        Daily,
+        EveryNumberOfDays,
+        OnCertainDaysOfTheWeek,
+    }
+
+    internal static class ScheduleTypeExtensions
+    {
+        public static string GetFriendlyName(this ScheduleType scheduleType)
+        {
+            switch (scheduleType)
+            {
+                case ScheduleType.Daily:
+                    return Properties.Resources.BreakScheduleDailyText;
+
+                case ScheduleType.EveryNumberOfDays:
+                    return Properties.Resources.BreakScheduleEveryNumberOfDaysText;
+
+                case ScheduleType.OnCertainDaysOfTheWeek:
+                    return Properties.Resources.BreakScheduleOnCertainDaysOfTheWeekText;
+
+                default:
+                    Debug.Fail(String.Format("Received unknown schedule type: {0}", scheduleType.ToString()));
+                    return scheduleType.ToString();
+            }
+        }
+    }
+}
