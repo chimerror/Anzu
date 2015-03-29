838abc94d19c61969a811ba3ab38ec34720820ee
foobardog <foobardog@hotmail.com>
2015-03-28 20:52:41 -0700

    Removing unused references.

--- Anzu/Badgerer/Properties/AssemblyInfo.cs
+++ Anzu/Badgerer/Properties/AssemblyInfo.cs
@@ -34,6 +34,6 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.15087.0620")]
-[assembly: AssemblyFileVersion("0.0.15087.0620")]
+[assembly: AssemblyVersion("0.0.15088.0339")]
+[assembly: AssemblyFileVersion("0.0.15088.0339")]
 [assembly: AssemblyInformationalVersion("0.0 Ashy Aphid - Alpha")]
